a85e9819dacc7dbec66c76571eaa1c75c585493e
Philip Zhang <zhangxilai@doublechaintech.com>
2025-03-31 16:27:13 +0800

    🚀add script

--- reload-from-git.sh
+++ reload-from-git.sh
@@ -1,5 +1,4 @@
 git pull 
-#docker restart  github-event-openresty-openresty-1
-
-docker exec github-event-openresty-openresty-1 openresty -s reload
+docker restart  github-event-openresty-openresty-1
+#docker exec github-event-openresty-openresty-1 openresty -s reload
 curl http://localhost:8680/hello
